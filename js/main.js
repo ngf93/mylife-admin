985c991d142b56d2d7042544f04e8448522ac7a4
ngf93 <nabiullinaguzel93@gmail.com>
2021-02-05 18:12:04 +0300

    3 pages

--- js/main.js
+++ js/main.js
@@ -93,8 +93,20 @@ $(document).ready( function() {
   $("#year").html(cur_year);
 });
 
-
-
+$(function() {
+  $(".quantity-arrow-plus").click(function() {
+      var num = $(this).prev(".quantity-num");
+      var num_val = num.val();
+      num.val(+num_val + 1);
+  });
+  $(".quantity-arrow-minus").click(function() {
+      var num = $(this).next(".quantity-num");
+      var num_val = num.val();
+      if (num_val > 1) {
+        num.val(+num_val - 1);
+      }
+  });
+});
 
 
 /* Left Menu on desktop */
@@ -205,3 +217,29 @@ $(function () {
     $(this).toggleClass('clicked');
   });
 })
+
+$(document).ready(function () {
+  bsCustomFileInput.init()
+})
+
+$('.input-upload').on('change', function() {
+  var arrayFiles = this.files, // массив с выбранными фалами
+      formItem = this.parentNode, // родительский элемент, для того чтобы вставить список с файлами
+      listFiles = document.createElement('ul'), // список с файлами
+      li = ''; // файлы
+  
+  // Если список с файлами уже вставлен в ДОМ, то удаляем его
+  if (formItem.querySelector('.list-files')) {
+    formItem.querySelector('.list-files').remove();
+  }
+  
+  listFiles.className = 'list-files'; // добавим класс, чтобы было удобнее стилять
+  
+  for (var i = 0; i < arrayFiles.length; i++) {
+    li += '<li>' + arrayFiles[i].name + '</li>'; // <li>Имя файла</li>
+  }
+  
+  listFiles.innerHTML = li;
+  
+  formItem.appendChild(listFiles);  
+});
